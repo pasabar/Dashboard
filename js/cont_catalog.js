c68512373fab953c5aba0786782883dcb5e3f1e2
fahad17 <fahadra96@gmail.com>
2023-12-04 00:47:07 +0700

    Fix authentication error and update table ID in katalog.html

--- js/cont_catalog.js
+++ js/cont_catalog.js
@@ -1,86 +1,58 @@
 const getTokenFromCookies = (cookieName) => {
-  const cookies = document.cookie.split(';')
+  const cookies = document.cookie.split(';');
   for (const cookie of cookies) {
-    const [name, value] = cookie.trim().split('=')
+    const [name, value] = cookie.trim().split('=');
     if (name === cookieName) {
-      return value
+      return value;
     }
   }
-  return null
-}
+  return null;
+};
 
-const getCatalogData = async () => {
-  const targetURL = 'https://asia-southeast2-pasabar.cloudfunctions.net/GetAll-dataCatalog' // Replace with your actual API endpoint
+const getAllCatalog = async () => {
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Authentication Error',
+      text: 'You are not logged in.',
+    }).then(() => {
+      window.location.href = 'pages/signin.html'; 
+    });
+    return;
+  }
+
+  const targetURL = 'https://asia-southeast2-pasabar.cloudfunctions.net/GetAll-dataCatalog';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
 
   const requestOptions = {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: myHeaders,
     redirect: 'follow',
-  }
+  };
 
   try {
-    const response = await fetch(targetURL, requestOptions)
-    const data = await response.json()
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
 
-    if (data.status === true) {
-      displayCatalogData(data.data, 'catalogDataBody')
+    if (data.status === 200) {
+      displayCatalogData(data.data, 'CatalogDataBody');
     } else {
       Swal.fire({
         icon: 'error',
         title: 'Error',
         text: data.message,
-      })
+      });
     }
   } catch (error) {
-    console.error('Error:', error)
+    console.error('Error:', error);
   }
-}
+};
 
-// Displaying data in the table
-const displayCatalogData = (catalogData, tableBodyId) => {
-  const catalogdataBody = document.getElementById(tableBodyId)
+// Add other functions here
 
-  catalogdataBody.innerHTML = ''
-
-  if (catalogData && catalogData.length > 0) {
-    catalogData.forEach((catalog) => {
-      const newRow = document.createElement('tr')
-      newRow.innerHTML = `
-        <td class="px-4 py-3">
-          <div class="flex items-center text-sm">
-            <div>
-              <p class="font-semibold">${catalog.nomorid}</p>
-            </div>
-          </div>
-        </td>
-        <td class="px-4 py-3 text-sm">
-          <p class="font-semibold">${catalog.title}</p>
-        </td>
-        <td class="px-4 py-3 text-sm">
-          <p class="font-semibold">${catalog.description}</p>
-        </td>
-        <td class="px-4 py-3 text-sm">
-          <img src="${catalog.image}" alt="Catalog Image" class="max-w-full h-auto">
-        </td>
-        <td class="px-4 py-3 text-xs">
-          <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-            ${catalog.status}
-          </span>
-        </td>
-        <td class="px-4 py-3">
-          <a href="#" class="edit-link" data-catalogid="${catalog.nomorid}">Edit</a>
-          <a href="#" class="delete-link" data-catalogid="${catalog.nomorid}">Delete</a>
-        </td>
-      `
-
-      catalogBody.appendChild(newRow)
-    })
-  } else {
-    catalogBody.innerHTML = `<tr><td colspan="6">No catalog data found.</td></tr>`
-  }
-}
-
-// Initial fetch of catalog data
-getCatalogData()
+// Initial fetch of all catalogs
+getAllCatalog();
